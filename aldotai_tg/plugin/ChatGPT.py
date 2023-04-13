d794111269033b09279947cce7f1771f79ffad3d
kaixinol <keiplyer@gmail.com>
2023-04-13 16:46:26 +0800

    typo

--- aldotai_tg/plugin/ChatGPT.py
+++ aldotai_tg/plugin/ChatGPT.py
@@ -47,7 +47,7 @@ async def chat(msg: str, usr_id: str) -> dict:
         for i in data_set[usr_id]:
             token += round(len(i["content"]) * 2)
         if token > 5000:
-            return {"msg": "Token数量过多，尝试使用/forgetme", "error": False}
+            return {"msg": "Token数量过多，尝试使用 /forgetme", "error": False}
     if tg_id in usage_limit and usage_limit[tg_id] > 32 and tg_username != config['admin']:
         return {"msg": "您的每日使用次数已用尽（32次）", "error": False}
     logger.info(f"<ID:{usr_id}>: {msg}")
